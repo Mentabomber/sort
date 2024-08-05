221fb231683aa3d750803ff25efea08020135054
Mentabomber <mentae_mentology@libero.it>
2024-08-05 17:45:47 +0200

    sorter class sort function without inferfaces

--- build/index.js
+++ build/index.js
@@ -7,11 +7,17 @@ class Sorter {
         const { length } = this.collection;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                //All of this only works if collection  is number[]
+                // If collection is an array of numbers
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
                 }
+                //Only going to work if collection is a string
+                //If collection is a sting, do this logic instead:
             }
         }
         // function swap(arr: any, index1: any, index2: any) {
